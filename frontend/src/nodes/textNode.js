a7eac4345b03ee40129bb4052964badaf1396b9e
pushkar <pushkar@waybeyond.tech>
2026-05-29 00:30:45 +0530

    complete task_3.1

--- frontend/src/nodes/textNode.js
+++ frontend/src/nodes/textNode.js
@@ -1,13 +1,22 @@
 // textNode.js
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Handle, Position } from 'reactflow';
 import { BaseNode } from "./BaseNode.js";
 
 export const TextNode = ({ id, data }) => {
   const [currText, setCurrText] = useState(data?.text || '{{input}}');
 
+  const textareaRef = useRef(null);
+
   const handleTextChange = (e) => {
+    // adjust height automatically
+    //? reset height on delete
+    textareaRef.current.style.height = 'auto';
+    //? increase height when text exceeds current height
+    textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    
+    // update the node data
     setCurrText(e.target.value);
   };
 
@@ -20,10 +29,20 @@ export const TextNode = ({ id, data }) => {
       <div>
         <label>
           Text:
-          <input 
-            type="text" 
+          {/* change to textarea coz inputs only ever have one line */}
+          <textarea 
+            ref={textareaRef}
+            rows={2}
             value={currText} 
             onChange={handleTextChange} 
+            className='nowheel nodrag'
+            // nowheel -> prevent reactflow from capturing mousewheel events, allowing user to scroll vertically inside textarea
+            // nodrag -> prevent reactflow from capturing drag events from inside the textarea
+            style={{ 
+              resize: 'none',
+              overflowY: 'auto',
+              maxHeight: '300px'
+            }}
           />
         </label>
       </div>
